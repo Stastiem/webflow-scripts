cf1e7d747154a556c7c9e8cfd6a52e6cc428bdc6
Mozzhakov <imozzhakov@icloud.com>
2023-08-23 14:28:39 +0300

    added fixes to google script

--- staging-multi-step.js
+++ staging-multi-step.js
@@ -11,6 +11,11 @@ function restrictAge() {
 }
 restrictAge();
 
+// find script tag with google maps api and set its src attribute
+const googleMapsScript = document.getElementById("google-maps-api");
+googleMapsScript.src =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyCRMOibgbPFCnvJ1IZjVTIVgYPRE7pk0rE&callback=initAutocomplete&libraries=places";
+
 // google maps autocomplete for address
 let autocomplete;
 let countryInputField = document.querySelector("#Country");
