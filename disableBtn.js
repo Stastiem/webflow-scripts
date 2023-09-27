af8aa00053d10e74bd778be7fbc5694605cf2388
Mozzhakov <imozzhakov@icloud.com>
2023-09-27 13:23:09 +0300

    refactored validation function, main file divided for several small files, files that are not in use moved to notUsed folder

---
+++ disableBtn.js
@@ -0,0 +1,15 @@
+export function disableBtn(fill) {
+  fill = false;
+  //next button style
+  $('[data-form="next-btn"]').css({
+    opacity: "0.4",
+    "pointer-events": "none",
+  });
+  $('[data-form="next-btn"]').addClass("disabled");
+  //submit btn style
+  $('[data-form="submit-btn"]').css({
+    opacity: "0.4",
+    "pointer-events": "none",
+  });
+  $('[data-form="submit-btn"]').addClass("disabled");
+}
---
+++ validation.js
@@ -0,0 +1,129 @@
+import { disableBtn } from "./disableBtn.js";
+import { enableBtn } from "./enableBtn.js";
+
+let domainAllowed = true;
+
+export function validateInput(inputField) {
+  let emptyInputArr = [];
+  inputField.each(function (i) {
+    if ($(this).val() !== "") {
+      emptyInputArr = emptyInputArr.filter((y) => y.input !== i);
+    } else {
+      if (!emptyInputArr.find((y) => y.input === i)) {
+        emptyInputArr.push({ input: i });
+      }
+    }
+  });
+  const isInputFilled = emptyInputArr.length === 0;
+  return isInputFilled;
+}
+
+function validateEmail(email, blockDomain) {
+  const isValidEmailFormat = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,20})?$/.test(email);
+  const domainEntered = email.includes("@")
+    ? email.split("@")[1].split(".")[0]
+    : "";
+
+  let domainAllowed = true;
+  if (blockDomain !== undefined) {
+    const blockedDomains = blockDomain.split(",");
+    domainAllowed = !blockedDomains.some((x) => x.includes(domainEntered));
+  }
+
+  return isValidEmailFormat && domainAllowed;
+}
+
+export function validation(
+  x,
+  steps,
+  radioFilled,
+  inputFilled,
+  telFilled,
+  dateFilled,
+  selectFilled,
+  emailFilled,
+  fileFilled
+) {
+  if ($(steps[x]).data("card")) {
+    enableBtn();
+  }
+  let currentStep = $(steps[x]);
+  let textInput = currentStep.find('input[type="text"][required]:visible');
+  let selectInput = currentStep.find("select[required]:visible");
+  let emailInput = currentStep.find('input[type="email"]:visible');
+  let dateInput = currentStep.find('input[type="date"][required]:visible');
+  let telInput = currentStep.find('input[type="tel"][required]:visible');
+  let fileInput = currentStep.find('input[type="file"][required]:visible');
+  let radioInput = currentStep.find('input[type="radio"][required]:visible');
+
+  // radio button validation
+  if (currentStep.find(":input[required]").is('[type="radio"]')) {
+    if (radioInput.is(":checked")) {
+      radioFilled = true;
+    } else {
+      radioFilled = false;
+    }
+  }
+
+  // email input validation
+  emailInput.each(function () {
+    if ($(this).val() !== "") {
+      emailFilled = validateEmail($(this).val(), $(this).data("block-domain"));
+    } else {
+      emailFilled = false;
+    }
+  });
+
+  inputFilled = validateInput(textInput);
+  telFilled = validateInput(telInput);
+  dateFilled = validateInput(dateInput);
+  selectFilled = validateInput(selectInput);
+  fileFilled = validateInput(fileInput);
+
+  // Happens when clicking the "remove" button on the file upload
+  $(steps[x])
+    .find(".w-icon-file-upload-remove")
+    .on("click", function () {
+      disableBtn();
+    });
+
+  // Happens when clicking the "remove" button on the file upload
+  $(steps[x])
+    .find(".link-5.w-file-remove-link")
+    .on("click", function () {
+      disableBtn();
+    });
+
+  $(steps[x])
+    .find(".link-back.w-inline-block")
+    .on("click", function () {
+      enableBtn();
+      setAllChecksToTrue();
+    });
+
+  function setAllChecksToTrue() {
+    inputFilled = true;
+    dateFilled = true;
+    telFilled = true;
+    radioFilled = true;
+    emailFilled = true;
+    domainAllowed = true;
+    selectFilled = true;
+    fileFilled = true;
+  }
+
+  if (
+    inputFilled &&
+    dateFilled &&
+    telFilled &&
+    radioFilled &&
+    emailFilled &&
+    domainAllowed &&
+    selectFilled &&
+    fileFilled
+  ) {
+    enableBtn();
+  } else {
+    disableBtn();
+  }
+}
