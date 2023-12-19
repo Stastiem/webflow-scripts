e191ace78d00539b73b67b12587a2bcc86d70d79
Mozzhakov <imozzhakov@icloud.com>
2023-12-19 13:11:11 +0200

    added country autocomplete, book lang autocomplete, changed address autocomplete
    Changed only the staging file; the production script remains the same. Added book language detection.

---
+++ utils/get-unix-timestamp.js
@@ -0,0 +1,46 @@
+function getUnixTimestampFromAPI() {
+  return new Promise((resolve, reject) => {
+    fetch("https://worldtimeapi.org/api/timezone/Europe/Riga")
+      .then((response) => response.json())
+      .then((data) => {
+        const timestamp = data.unixtime;
+        resolve(timestamp);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+Webflow.push(function () {
+  $("form").submit(function (event) {
+    window.dataLayer.push({ event: "OrderFilled" });
+    event.preventDefault();
+
+    getUnixTimestampFromAPI()
+      .then((timestamp) => {
+        const website_url = "https://buy.stripe.com/4gw3ewb2C7PaaKQbII";
+        const ref_id =
+          $("#Email")
+            .val()
+            .replace(/@/g, "___")
+            .replace(/\./g, "__")
+            .toLowerCase() +
+          "_" +
+          timestamp;
+        setTimeout(function () {
+          location.href =
+            website_url +
+            "?prefilled_email=" +
+            $("#Email").val() +
+            "&client_reference_id=" +
+            ref_id +
+            "&locale=" +
+            $("#BookLanguage").val();
+        }, 100);
+      })
+      .catch((error) => {
+        console.error("Error fetching the timestamp:", error);
+      });
+  });
+});
---
+++ utils/weglot-initialize.js
@@ -0,0 +1,30 @@
+// init Weglot
+Weglot.initialize({
+  api_key: "wg_a24e3bfe3d34e7a4c7820d9f9274d1cd1",
+});
+
+// on Weglot init
+Weglot.on("initialized", () => {
+  // get the current active language
+  const currentLang = Weglot.getCurrentLang();
+  // if the current active language link exists
+  if (
+    document.querySelector(".wg-element-wrapper.sw3 [lang=" + currentLang + "]")
+  ) {
+    // click the link
+    document
+      .querySelector(".wg-element-wrapper.sw3 [lang=" + currentLang + "]")
+      .click();
+  }
+});
+
+// for each of the .wg-element-wrapper language links
+document.querySelectorAll(".wg-element-wrapper.sw3 [lang]").forEach((link) => {
+  // add a click event listener
+  link.addEventListener("click", function (e) {
+    // prevent default
+    e.preventDefault();
+    // switch current active language after a setTimeout
+    setTimeout(() => Weglot.switchTo(this.getAttribute("lang")), 700);
+  });
+});
