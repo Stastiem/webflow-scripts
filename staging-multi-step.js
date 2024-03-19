269f5237b38514e0421566de6239f3759359efe7
Mozzhakov <imozzhakov@icloud.com>
2024-03-19 12:58:06 +0200

    Update staging-multi-step.js

--- staging-multi-step.js
+++ staging-multi-step.js
@@ -579,16 +579,18 @@ async function autocompleteCountry() {
       );
 
       const select = document.getElementById("Country");
-      for (let i = 0; i < select.options.length; i++) {
-        const option = select.options[i];
-        if (option.value === userCountry) {
-          option.selected = true;
-          initAutocomplete(option.value);
-          if (option.value !== "lv" && option.value !== "") {
-            shippingBlock.style.display = "block";
+      if (!localStorage.getItem("filledInput")) {
+        for (let i = 0; i < select.options.length; i++) {
+          const option = select.options[i];
+          if (option.value === userCountry) {
+            option.selected = true;
+            initAutocomplete(option.value);
+            if (option.value !== "lv" && option.value !== "") {
+              shippingBlock.style.display = "block";
+            }
+            customizeShipping(option.value);
+            break;
           }
-          customizeShipping(option.value);
-          break;
         }
       }
     } else {
